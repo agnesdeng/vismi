fef9503badd5cfb4e6531f30125fba850b19e062
YD <25675889+agnesdeng@users.noreply.github.com>
2025-12-25 09:51:14 +1300

    ts

--- R/vismi-package.R
+++ R/vismi-package.R
@@ -11,14 +11,16 @@
 #' @importFrom utils modifyList
 #' @importFrom stats as.formula setNames density
 #' @importFrom dplyr group_by reframe tibble filter ungroup mutate summarise n sym
+#' @importFrom tidyr pivot_longer
+#' @importFrom purrr map
 #' @importFrom ggplot2 ggplot aes geom_histogram geom_density geom_point geom_bar geom_boxplot geom_jitter position_jitter margin unit
 #' @importFrom ggplot2 scale_y_continuous scale_y_discrete scale_alpha_discrete scale_color_manual scale_fill_manual ylab labs guides guide_legend theme element_text element_rect element_line
 #' @importFrom ggplot2 after_stat facet_grid vars labeller ylim coord_cartesian stat_qq stat_qq_line position_dodge ggplot_build
 #' @importFrom ggridges geom_density_ridges theme_ridges
-#' @importFrom tidyr pivot_longer
 #' @importFrom mixgb mixgb impute_new
 #' @importFrom rlang .data
 #' @importFrom scales hue_pal
+#' @importFrom trelliscopejs trelliscope
 #' @importFrom grDevices nclass.Sturges colorRampPalette
 #' @importFrom gridExtra arrangeGrob
 #' @importFrom grid textGrob gpar
