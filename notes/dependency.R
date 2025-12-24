455f1155a80a12ead41de9f6a7224894381f54ca
YD <25675889+agnesdeng@users.noreply.github.com>
2025-12-25 09:57:13 +1300

    add notes dev to gitignore

--- notes/dependency.R
+++
@@ -1,8 +0,0 @@
-if (!requireNamespace("midae", quietly = TRUE)) {
-  stop("Method 'midae' requires the 'miae' package.")
-}
-
-
-if (!requireNamespace("mice", quietly = TRUE)) {
-  stop("Method 'mice' requires the 'mice' package.")
-}
